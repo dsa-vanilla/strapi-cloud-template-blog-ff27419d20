3516434de79604464b44fa2243e3dc217183e9da
Marceli Janowski <Marcelli.Janowski@domesticandgeneral.com>
2024-08-02 13:19:10 +0100

    chore: chnage to structure to fit db

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      serverPublicHostname: 'https://brilliant-desire-259bc3d264.strapiapp.com/', // default: "".
+    },
+  },
+});
