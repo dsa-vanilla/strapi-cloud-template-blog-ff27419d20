afb8f20dd97f728889a67056fa594dd8bb3bd164
Marceli Janowski <Marcelli.Janowski@domesticandgeneral.com>
2024-08-03 22:21:21 +0100

    chore: remove import plugin

--- config/plugins.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  'import-export-entries': {
-    enabled: true,
-    config: {
-      serverPublicHostname: 'https://brilliant-desire-259bc3d264.strapiapp.com/', // default: "".
-    },
-  },
-});
