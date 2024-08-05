20c1890fb0c995b48316052f8e56fb7ccdd6f091
Marceli Janowski <Marcelli.Janowski@domesticandgeneral.com>
2024-08-05 09:04:53 +0100

    feat: remove carousel

--- src/api/carrousel/controllers/carrousel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * carrousel controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::carrousel.carrousel');
